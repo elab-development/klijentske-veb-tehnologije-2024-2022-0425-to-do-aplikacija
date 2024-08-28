a4cd3f990d3df4c3ce0be26ed0e0a5815f77e74b
sofyy <sofibursac@gmail.com>
2024-08-28 21:13:37 +0200

    Sitne ispravke

--- todoo-webapp/src/App.tsx
+++ todoo-webapp/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/ReusableComponents/Header.tsx';
 import Footer from './components/ReusableComponents/Footer.tsx';
 import About from './components/About/About.tsx';
+import Categories from './components/Categories/Categories.tsx';
+import Contact from './components/Contact/Contact.tsx';
 import { User } from './types.ts';
 import { useState } from 'react';
 
@@ -14,6 +16,8 @@ function App(){
       <Header user ={currentUser} setUser={setCurrentUser}/>
       <Routes>
         <Route path="/" element={<About />} />
+        <Route path="/kategorije" element={<Categories />} />
+        <Route path="/kontakt" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
