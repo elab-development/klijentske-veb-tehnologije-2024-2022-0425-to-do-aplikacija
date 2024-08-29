7adfc7d42616181e63a08529de41189e7ef828c0
elmin2110 <elmin.ahmeti@gmail.com>
2024-08-29 18:35:10 +0200

    docker editovan vite.config.ts

--- todoo-webapp/vite.config.ts
+++ todoo-webapp/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Port na kojem će server raditi
+    host: true // Dodaj ovo ako želiš da izložiš aplikaciju preko mreže
+  },
 })
