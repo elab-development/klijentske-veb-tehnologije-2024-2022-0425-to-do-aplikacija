2e3f0ffe6a845661a076e318a26eb21fa7f8d57e
elmin2110 <elmin.ahmeti@gmail.com>
2024-08-29 16:14:21 +0200

    ubacena paginacija

--- todoo-webapp/src/components/Daily/Daily.tsx
+++ todoo-webapp/src/components/Daily/Daily.tsx
@@ -1,16 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { UserProp } from "../../types";
 import ToDoList from "../ReusableComponents/ToDoList";
 
 
 function Daily({user,setUser}:UserProp) {
 
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
 
   return (
     <section>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ display: "flex", flexDirection:"column", justifyContent: "center",alignItems:"center" }}>
         <div style={{ width: "60vh", minHeight:"60vh" }}>
           <ToDoList name="DAILY" user={user} setUser={setUser} ></ToDoList>
         </div>
+        <div><span>1 </span><a><span onClick={() => handleNavigation('/travel')}>{">"}</span></a></div>
       </div>
     </section>
   );
--- todoo-webapp/src/components/Travel/Travel.tsx
+++ todoo-webapp/src/components/Travel/Travel.tsx
@@ -1,15 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { UserProp } from "../../types";
 import ToDoList from "../ReusableComponents/ToDoList";
 
 
 function Travel({user,setUser}:UserProp) {
 
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <section>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ display: "flex", flexDirection:"column", justifyContent: "center",alignItems:"center" }}>
         <div style={{ width: "60vh", minHeight:"60vh" }}>
-          <ToDoList name="TRAVEL" user={user} setUser={setUser}></ToDoList>
+          <ToDoList name="TRAVEL" user={user} setUser={setUser} ></ToDoList>
         </div>
+        <div><a><span onClick={() => handleNavigation('/daily')}>{"< "}</span></a><span> 2</span></div>
       </div>
     </section>
   );
